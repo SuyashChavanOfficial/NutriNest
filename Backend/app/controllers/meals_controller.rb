335b5b1947564dca63b2ebcee7864fedc8fb31d6
Suyash Chavan <suyash.chavan@tudip.com>
2026-03-20 15:12:06 +0530

    feat: add endpoint to get today's meal - get today's meal only - improve route stucture

--- Backend/app/controllers/meals_controller.rb
+++ Backend/app/controllers/meals_controller.rb
@@ -1,5 +1,4 @@
 class MealsController < ApplicationController
-
   def create
     meal = current_user.meals.new(meal_params)
 
@@ -38,11 +37,33 @@ class MealsController < ApplicationController
     render json: formatted_meals
   end
 
+  def today
+    meals = policy_scope(Meal)
+              .where(date: Date.today)
+              .includes(:meal_items)
+
+    formatted_meals = meals.map do |meal|
+      {
+        type: meal.meal_type.capitalize || "Unknown",
+        calories: meal.meal_items.sum(:calories),
+        date: meal.date,
+        created_at: meal.created_at,
+        items: meal.meal_items.map do |item|
+          {
+            name: item.food_name&.capitalize || "Unknown",
+            calories: item.calories || 0
+          }
+        end
+      }
+    end
+
+    render json: formatted_meals
+  end
+
 
   private
 
   def meal_params
     params.permit(:meal_type, :date)
   end
-
 end
--- Backend/config/routes.rb
+++ Backend/config/routes.rb
@@ -5,24 +5,24 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+# Defines the root path route ("/")
+# root "posts#index"
 
 post "/signup", to: "auth#signup"
 post "/signin", to: "auth#signin"
 
-post "/goals", to: "goals#create"
-get "/goals", to: "goals#show"
 patch "/edit_profile", to: "users#update"
 
+resources :goals, only: [ :create, :show ]
+
+
 resources :meals do
-  resources :meal_items, only: [:create, :index]
+  resources :meal_items, only: [ :create, :index ]
+  collection do
+    get :today
+  end
 end
-
-resources :workouts, only: [:create, :index]
-
 delete "/meal_items/:id", to: "meal_items#destroy"
 
-
+resources :workouts, only: [ :create, :index ]
 end
-
